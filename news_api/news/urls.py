88fb65d75a6a381e5d554b3e9083f705410d69b5
fkilld <fkill.d.azadd@gmail.com>
2025-11-15 11:14:58 +0530

    Update URL patterns to include detail, draft, and update views for news articles

--- news_api/news/urls.py
+++ news_api/news/urls.py
@@ -6,6 +6,10 @@ from .views import *
 urlpatterns = [
     path('',NewsListView.as_view(), name='news-list'),
     path('create/', NewsCreateView.as_view(), name='news-create'),
+    # /api/news/{slug}/
+    path('<slug:slug>/', NewsDetailView.as_view(), name='news-detail'),
+    path('my/drafts/', MyDraftNewsListView.as_view(), name='my-draft-news-list'),
+    path('<slug:slug>/update/', NewsUpdateView.as_view(), name='news-update')
 
 
 ]
